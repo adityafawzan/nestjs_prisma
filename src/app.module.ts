afa17df272e29940f5674bf19284615c610f793b
adityafawzan <fawzan89@gmail.com>
2022-07-19 17:08:46 +0700

    19-07-22-1708

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { GiftsModule } from './gifts/gifts.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, GiftsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    GiftsModule,
+  ],
   controllers: [],
   providers: [],
 })

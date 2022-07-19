afb66a609a9fb42120e91518d641213e54e2254b
adityafawzan <fawzan89@gmail.com>
2022-07-19 12:54:38 +0700

    19-07-22-1254

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const config = new DocumentBuilder()
-    .setTitle('Median')
-    .setDescription('The Median API description')
+    .setTitle('RollingGlory')
+    .setDescription('The RollingGlory API description')
     .setVersion('0.1')
     .build();
 
